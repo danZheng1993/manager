11e2c14f7cebcaf057e438263a335e1418e554db
Daniel Zheng <dannyzheng1993@gmail.com>
2020-07-25 00:06:14 +0800

    WIP

--- src/redux/api/apiCall.js
+++ src/redux/api/apiCall.js
@@ -15,7 +15,8 @@ import {
 const defaultHeaders = () => {
   const auth = localStorage.getItem('hvr_auth')
   //axios.defaults.baseURL = process.env.API_ROOT + '/'
-  axios.defaults.baseURL = 'http://192.168.1.86:4000/'
+  // axios.defaults.baseURL = 'http://192.168.1.86:4000/'
+  axios.defaults.baseURL = 'http://localhost:4000/'
   let headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
